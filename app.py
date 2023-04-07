cc15b0c18b826b067c4ab06b06758f3f441467d2
sameeriron42 <sameermark42@gmail.com>
2023-04-07 17:46:14 +0530

    proper status codes

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def home():
     return Response(response=json.dumps({"status":"up"}),status=200,mimetype='application/json')
 
 @app.route('/users', methods=['GET'])
+#returns names of all users in database
 def get_all_users():
     output= user_resource.find({},{"name":1})
     list_of_users = []
@@ -22,6 +23,7 @@ def get_all_users():
     return Response(response=json.dumps(list_of_users),status=200,mimetype='application/json')
 
 @app.get('/users/<id>')
+#returns document associated with <id>
 def get_user(id):
     try:
         result = user_resource.find_one({"_id": ObjectId(id)})
@@ -34,17 +36,19 @@ def get_user(id):
         return Response(response=json.dumps(json_result),status=200,mimetype='application/json')
 
 @app.route('/users', methods=['POST'])
+#create new document
 def create_user():
-    
     data = request.json
+    #skipping data validation as DB schema is missing
     if(data=={}):
-        return Response(response='bad request',status=404)
+        return Response(response='Bad request, data cannot be empty',status=404)
+
     result = user_resource.insert_one(data)
     if(result.acknowledged==False):
-        return Response(status=400)
+        return Response(status=404)
     output = {'inserted_id':str(result.inserted_id)}
 
-    return Response(response = json.dumps(output),status=200,mimetype='application/json')
+    return Response(response = json.dumps(output),status=201,mimetype='application/json')
 
 @app.put('/users/<id>')
 def update(id):
@@ -56,23 +60,23 @@ def update(id):
         return Response(response=str(e),status=400,mimetype='text')
     
     if(result.matched_count == 0):
-        return Response(response='no record with specified id found',status=400)
+        return Response(response='no record with specified id found',status=404)
     if(result.modified_count == 0):
         return Response(response='no modifications made',status=200)
     
     updatedResult = user_resource.find_one({"_id":ObjectId(id)})
     json_result = {"name": updatedResult['name'],"email":updatedResult['email'],"password":updatedResult['password']}
 
-    return Response(response=json.dumps(json_result),status=200,mimetype='application/json')
+    return Response(response=json.dumps(json_result),status=201,mimetype='application/json')
     
 @app.delete('/users/<id>')
 def delete(id):
     try:
-        delete_result = user_resource.delete_one({"_id":ObjectId(id)},{})
+        delete_result = user_resource.delete_one({"_id":ObjectId(id)})
     except Exception as e:
         return Response(response=str(e),status=400,mimetype='text')
     if(delete_result.deleted_count == 0):
-        return Response(response='Nothing to DELETE, no record found with this ID',status=400)
+        return Response(response='Nothing to DELETE, no record found with this ID',status=404)
 
     return Response(response='Deleted document with id: '+id,status=200)
 
